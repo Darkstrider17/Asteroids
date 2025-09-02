4114a5cfd187d35b831eeb3a113979ddb7dd8714
Darkstrider17 <darkstridergithub@gmail.com>
2025-09-02 20:41:40 +1000

    added player shape and begun movement

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -19,9 +22,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-    screen.fill((0,0,0))
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000  # limits the framerate to 60 fps
+        screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000  # limits the framerate to 60 fps
 
 if __name__ == "__main__":
     main()
